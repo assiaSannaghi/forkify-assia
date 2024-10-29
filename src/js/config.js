49889d478cb698a70f3461b720a2892157ba925d
assiaSannaghi <assia.sannaghi>
2024-10-29 16:22:17 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b83b3de6-f656-4eb4-82ba-6fc15a7f0d67';
+export const MODAL_CLOSE_SEC = 2.5;
